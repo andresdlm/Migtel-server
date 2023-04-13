17cc6395444fc4e7a05b7875b9b49b48f2ba8264
Andres Linarez <andreslinarez9@gmail.com>
2023-04-13 12:45:56 -0400

    Librerias actualizadas

--- src/reports/controllers/reports.controller.ts
+++ src/reports/controllers/reports.controller.ts
@@ -6,18 +6,4 @@ import { ReportsService } from '../services/reports.service';
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
-
-  @Post('libroventas.pdf')
-  async getFile(@Body() payload: SalesBookDto): Promise<StreamableFile> {
-    const buffer = await this.reportsService.generateSalesBookPDF(payload);
-
-    return new StreamableFile(buffer);
-  }
-
-  @Post('paymentReport.pdf')
-  async paymentReport(@Body() payload: ReportDto): Promise<StreamableFile> {
-    const buffer = await this.reportsService.generatePaymentReport(payload);
-
-    return new StreamableFile(buffer);
-  }
 }
