e405dfcc9eabd4aa53bd70a2a31add183649f9a0
Andres Linarez <andreslinarez9@gmail.com>
2023-11-14 12:05:59 -0400

    Security patches: report services protection

--- src/reports/controllers/reports.controller.ts
+++ src/reports/controllers/reports.controller.ts
@@ -1,61 +1,78 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+
 import {
   PaymentReportDto,
   ReferenceDto,
   ReportDto,
   SalesBookReportDto,
 } from '../dtos/reports.dtos';
-import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ReportsService } from '../services/reports.service';
+import { ApiKeyGuard } from 'src/auth/guards/api-key.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/models/roles.model';
 
+@UseGuards(ApiKeyGuard, JwtAuthGuard, RolesGuard)
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('salesBook')
   async getSalesBookReport(@Body() payload: SalesBookReportDto) {
     return await this.reportsService.getSalesBookReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('accountReport')
   async getAllAccount(@Body() payload: ReportDto) {
     return await this.reportsService.getAccountReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('accountPaymentReport')
   async getAccountPaymentReport(@Body() payload: ReportDto) {
     return await this.reportsService.getAccountPaymentReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('paymentReport')
   async getPaymentReport(@Body() payload: PaymentReportDto) {
     return await this.reportsService.getPaymentReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('retentionsReport')
   async getRetentionsReport(@Body() payload: ReportDto) {
     return await this.reportsService.getRetentionsReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('referenceInvoiceReport')
   async getReferencePaymentReport(@Body() payload: ReferenceDto) {
     return await this.reportsService.getReferenceInvoiceReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('referencePaymentReport')
   async getReferenceInvoiceReport(@Body() payload: ReferenceDto) {
     return await this.reportsService.getReferencePaymentReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('igtfBook')
   async getIgtfBookReport(@Body() payload: ReportDto) {
     return await this.reportsService.getIgtfBookReport(payload);
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Get('paidInvoiceReport')
   async getPaidInvoiceReport() {
     return await this.reportsService.getPaidInvoiceReport();
   }
 
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
   @Post('conciliationInvoice')
   async getConciliationInvoice(@Body() payload: SalesBookReportDto) {
     return await this.reportsService.getConciliationReport(payload);
