2ebed6d8c751940246fd5aa931241f2766ea18d5
Andres Linarez <andreslinarez9@gmail.com>
2024-05-08 11:07:38 -0400

    Dashboard bug fixed

--- src/dashboard/entities/monthlyIncomeView.entity.ts
+++ src/dashboard/entities/monthlyIncomeView.entity.ts
@@ -4,26 +4,43 @@ import { ViewEntity, ViewColumn } from 'typeorm';
 @ViewEntity({
   expression: `
     SELECT
+      TRIM(TO_CHAR(DATE_TRUNC('month', register_date), 'Month')) AS month_string,
+      EXTRACT(MONTH FROM DATE_TRUNC('month', register_date))::integer AS month_number,
+      COUNT(*)::integer AS invoice_count,
+      ROUND(COALESCE(sum(
+      CASE
+        WHEN invoices.currency_code::text = 'BS'::text
+          THEN invoices.subtotal / invoices.exhange_rate
+        ELSE invoices.subtotal
+      END), 0::numeric), 2) AS monthly_income,
       ROUND(COALESCE(sum(
         CASE
           WHEN invoices.currency_code::text = 'BS'::text
             THEN invoices.iva / invoices.exhange_rate
           ELSE invoices.iva
-        END::numeric), 0::numeric), 2) AS monthly_taxes,
-      ROUND(COALESCE(sum(
-        CASE
-          WHEN invoices.currency_code::text = 'BS'::text
-            THEN invoices.subtotal / invoices.exhange_rate
-          ELSE invoices.subtotal
-        END::numeric), 0::numeric), 2) AS monthly_income
-    FROM invoices
-    WHERE invoices.type::text = 'FACT'::text
-      AND invoices.canceled = false
-      AND date_part('month'::text, invoices.register_date) = date_part('month'::text, now())
+        END), 0::numeric), 2) AS monthly_taxes
+    FROM
+      invoices
+    WHERE
+      register_date >= CURRENT_DATE - INTERVAL '12 months' AND
+      invoices.type = 'FACT'
+    GROUP BY
+      DATE_TRUNC('month', register_date)
+    ORDER BY
+      DATE_TRUNC('month', register_date);
   `,
   name: 'monthly_income_view',
 })
 export class MonthlyIncomeView {
+  @ViewColumn({ name: 'month_string' })
+  monthString: string;
+
+  @ViewColumn({ name: 'month_number' })
+  monthNumber: number;
+
+  @ViewColumn({ name: 'invoice_count' })
+  invoiceCount: number;
+
   @ViewColumn({
     name: 'monthly_income',
     transformer: new ColumnNumericTransformer(),
--- src/dashboard/services/dashboard.service.ts
+++ src/dashboard/services/dashboard.service.ts
@@ -5,7 +5,6 @@ import { Repository } from 'typeorm';
 import { DailyIncomeView } from '../entities/dailyIncomeView.entity';
 import { MonthlyAccountIncomeView } from '../entities/monthlyAccountIncomeView.entity';
 import { MonthlyIncomeView } from '../entities/monthlyIncomeView.entity';
-import { MonthlySalesView } from '../entities/monthlySalesView.entity';
 import { CurrencyRateService } from '../../currency-rate/services/currency-rate.service';
 
 @Injectable()
@@ -17,30 +16,20 @@ export class DashboardService {
     private monthlyAccountIncomeViewRepo: Repository<MonthlyAccountIncomeView>,
     @InjectRepository(MonthlyIncomeView)
     private monthlyIncomeViewRepo: Repository<MonthlyIncomeView>,
-    @InjectRepository(MonthlySalesView)
-    private monthlySalesViewRepo: Repository<MonthlySalesView>,
     private currencyRateService: CurrencyRateService,
   ) {}
 
   async generateDashboard() {
     const [dailyIncome] = await this.dailyIncomeViewRepo.find();
-    const [monthlyIncome] = await this.monthlyIncomeViewRepo.find();
-    const monthlySalesIncome = await this.monthlySalesViewRepo.find();
+    const monthlyIncome = await this.monthlyIncomeViewRepo.find();
     const monthlyAccountIncome = await this.monthlyAccountIncomeViewRepo.find();
     const currencyRate = await this.currencyRateService.getLatestUsdRate();
 
     const dashboard = {
       dailyIncome: dailyIncome?.dailyIncome || 0,
-      monthlyIncome: monthlyIncome?.monthlyIncome || 0,
-      monthlyTaxes: monthlyIncome?.monthlyTaxes || 0,
-      yearlyIncome: monthlySalesIncome.map((income) => income.monthlyTotal),
       currencyRate: currencyRate.price,
-      accountsBalance: monthlyAccountIncome.map((income) => ({
-        id: income.id,
-        name: income.name,
-        count: income.count,
-        y: income.y,
-      })),
+      monthlyStats: monthlyIncome,
+      accountsBalance: monthlyAccountIncome,
     };
 
     return dashboard;
