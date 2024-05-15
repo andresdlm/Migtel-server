4c58c83968dd268f86414aa9577f1cc3b12e2986
Andres Linarez <andreslinarez9@gmail.com>
2024-05-15 11:17:05 -0400

    Communicator role added

--- src/auth/models/roles.model.ts
+++ src/auth/models/roles.model.ts
@@ -2,5 +2,6 @@ export enum Role {
   SUPER_ADMIN = 'super_admin',
   ADMIN = 'admin',
   OPERATOR = 'operator',
+  COMMUNICATOR = 'communicator',
   READER = 'reader',
 }
--- src/notify-api/controllers/notify.controller.ts
+++ src/notify-api/controllers/notify.controller.ts
@@ -19,37 +19,37 @@ import { FilterByTags } from '../dtos/filter.dtos';
 export class NotifyController {
   constructor(private notifyService: NotifyService) {}
 
-  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.COMMUNICATOR)
   @Post('singleSMS')
   singleSMS(@Body() payload: SingleSMSDTO) {
     return this.notifyService.singleSMS(payload);
   }
 
-  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.COMMUNICATOR)
   @Post('massSMS')
   massSMS(@Body() payload: MassSMSDTO) {
     return this.notifyService.massSMS(payload);
   }
 
-  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.COMMUNICATOR)
   @Post('paymentRecievedSMS')
   paymentRecievedSMS(@Body() payload: PaymentRecievedSMSDTO) {
     return this.notifyService.paymentRecievedSMS(payload);
   }
 
-  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.COMMUNICATOR)
   @Get('getClientTags')
   getClientTags() {
     return this.notifyService.getClientTags();
   }
 
-  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.COMMUNICATOR)
   @Post('getFilteredClients')
   getFilteredClients(@Body() payload: FilterByTags) {
     return this.notifyService.getFilteredClients(payload);
   }
 
-  @Roles(Role.SUPER_ADMIN, Role.ADMIN)
+  @Roles(Role.SUPER_ADMIN, Role.ADMIN, Role.COMMUNICATOR)
   @Post('massByTagSMS')
   massByTagSMS(@Body() payload: MassByTagDTO) {
     return this.notifyService.massByTagSMS(payload);
