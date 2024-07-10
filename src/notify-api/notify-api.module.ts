16c5caa8e3348f580c602a5f154dd6dc73a80b77
taneralberto <taneralberto@gmail.com>
2024-07-10 16:56:11 -0400

    websocket connection to notify backend and emit to fact front created

--- src/notify-api/notify-api.module.ts
+++ src/notify-api/notify-api.module.ts
@@ -5,11 +5,12 @@ import { NotifyController } from './controllers/notify.controller';
 import { NotifyService } from './services/notify.service';
 import { LoggerModule } from 'src/logger/logger.module';
 import { BlacklistService } from './services/blacklist.service';
+import { NotifyGateway } from './gateways/notify.gateway';
 
 @Module({
   imports: [HttpModule, LoggerModule],
   controllers: [NotifyController],
-  providers: [NotifyService, BlacklistService],
-  exports: [NotifyService, BlacklistService],
+  providers: [NotifyService, BlacklistService, NotifyGateway],
+  exports: [NotifyService, BlacklistService, NotifyGateway],
 })
 export class NotifyApiModule {}
